b7617d284f8986c303da4498e479e9e9fc69383e
Ntebaleng Elsie Letaoana <146706453+NtebalengL@users.noreply.github.com>
2025-09-08 00:22:12 +0200

    Update clinics.js

--- front_end/pages/clinics.js
+++ front_end/pages/clinics.js
@@ -1,1 +1,13 @@
+export default function Clinics() {
+  return (
+    <div className="space-y-4">
+      <h2 className="text-2xl font-bold">Clinics & Helplines</h2>
+      <ul className="list-disc list-inside space-y-2">
+        <li><a href="tel:10111">SAPS Emergency: 10111</a></li>
+        <li><a href="tel:116">Childline: 116</a></li>
+        <li><a href="https://www.tears.co.za/" target="_blank">TEARS Foundation</a></li>
+      </ul>
+    </div>
+  )
+}
 
